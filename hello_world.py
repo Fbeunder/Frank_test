788ebdbcd3dc6610f248470209f4fe2ca1dc9d59
Fbeunder <frank@lynxx.eu>
2025-02-26 05:47:49 +0100

    Add hello_world.py script

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    """
+    Een eenvoudige functie die 'Hello, World!' print
+    """
+    print("Hello, World!")
+    print("Welkom bij mijn eerste Python script in de Frank_test repository!")
+
+if __name__ == "__main__":
+    # Dit zorgt ervoor dat main() alleen wordt uitgevoerd als dit script direct wordt gestart
+    # en niet als het wordt geïmporteerd als module in een ander script
+    main()
